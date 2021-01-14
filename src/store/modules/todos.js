704e9b3da50714c886352fe00b3a8165e666abc3
Héctor Espinoza <hespinoza@uchile.cl>
2021-01-14 20:59:06 -0300

    first commit

---
+++ src/store/modules/todos.js
@@ -0,0 +1,25 @@
+
+const state = {
+    todos: [{
+        id: 1,
+        title: 'Todo one'
+    }, {
+        id: 2,
+        title: 'Todo two'
+    }]
+};
+
+const getters = {
+    allTodos: (state) => state.todos
+};
+
+const actions = {};
+
+const mutations = {};
+
+export default {
+    state,
+    getters,
+    actions,
+    mutations
+};
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <Todos />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Todos from "@/components/Todos.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Todos,
+  },
+};
+</script>
