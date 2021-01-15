52c0806919c0f336dbe87fd116f9d9aa0629290d
OscvrRojvs <orojasmira@gmail.com>
2021-01-14 21:20:37 -0300

    registro de integrantes en vista home

---
+++ src/components/Integrantes.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <ul>
+      <li>Hector Espinoza</li>
+      <li>Oscar Rojas</li>
+      <li>Matias Romero</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Integrantes'
+}
+</script>
