f2dc4b6d19287f224eb241245c2086e21bfd9473
OscvrRojvs <60526323+OscvrRojvs@users.noreply.github.com>
2021-01-14 21:36:03 -0300

    registro de integrantes en vista home (#3)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="home">
     <Todos />
+    <Integrantes />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Todos from "@/components/Todos.vue";
-
+import Integrantes from '../components/Integrantes'
 export default {
   name: "Home",
   components: {
     Todos,
+    Integrantes
   },
 };
 </script>
